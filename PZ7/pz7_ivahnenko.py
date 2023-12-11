6aa260f86a53474c02b533b586b80798d82d3de4
ShaobiShain <anatolyivahnenko@yandex.ru>
2023-12-11 19:52:02 +0300

    last commit 3

--- PZ7/pz7_ivahnenko.py
+++ PZ7/pz7_ivahnenko.py
@@ -18,10 +18,10 @@ def alternating_chars(N, C1, C2):
     return result
     
 
-N = 10  
+N = 9  
 symbol1 = 'Hello'  
 symbol2 = 'World' 
-result_string = alternating_chars(N, symbol1, symbol2)# Вывод чепед. символо
+result_string = alternating_chars(N, symbol1, symbol2)# Вывод черед. символов
 print(result_string)
 
 
@@ -32,17 +32,24 @@ print(result_string)
 # (путь), собственно имя и расширение. Выделить из этой строки имя файла (без
 # расширения).
 
+
+# способ 1
     
-full_file_name = 'hello_world.txt'  
-# Разделение имени файла и расширения
-file_name, file_extension = full_file_name.split('.')
+# full_file_name = 'C:\\Users\\Анатолий\\Desktop\\clone_pract\\PZ\\PZ7\\hello_world.txt'
+
+# # Получение имени файла без расширения
+# file_extension = full_file_name.split('\\')[-1]
+# file_name = file_extension.split('.')[0]
+
+# print("Имя файла без расширения:", file_name)
+
+# способ 2
+
+import os
+full_file_name = r'C:\Users\Анатолий\Desktop\clone_pract\PZ\PZ7\hello_world.txt'
+# Получение имени файла без расширения
+file_name = os.path.splitext(os.path.basename(full_file_name))[0]
 print("Имя файла без расширения:", file_name)
-
-
-
-
-
-
 
 
 
