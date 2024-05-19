cb1b179e812c5d893ca1108c3accf80117c6a96d
ShaobiShain <anatolyivahnenko@yandex.ru>
2024-05-19 11:04:29 +0300

    commit 27

--- PZ16/pz16_p3.py
+++ PZ16/pz16_p3.py
@@ -1,38 +1,93 @@
-# Для задачи из блока 1 создать две функции, save_def и load_def, которые позволяют
-# сохранять информацию из экземпляров класса (3 шт.) в файл и загружать ее обратно.
-# Использовать модуль pickle для сериализации и десериализации объектов Python в
-# бинарном формате.
+# Для задачи из блока 1 создать две функции, save_def и load_def, которые позволяют сохранять информацию из экземпляров класса (3 шт.)
+# в файл и загружать ее обратно. Использовать модуль pickle для сериализации и десериализации объектов Python в бинарном формате.
 
 import pickle
+from colorama import init, Fore, Back, Style
 
-class MyClass:
-    def __init__(self, attribute1, attribute2):
-        self.attribute1 = attribute1
-        self.attribute2 = attribute2
+init()
 
-def save_def(obj, filename):
-    with open(filename, 'wb') as f:
-        pickle.dump(obj, f)
+print(Style.BRIGHT + Fore.GREEN)
+print("+──────────────────────────────────────────────────────── +\n| ╔╗╔══╗╔══╗ ╔══╗╔╗   ╔╗╔══╗╔╗╔╗ ╔══╗╔═══╗╔════╗╔══╗╔═══╗ |\n| ║║║╔═╝║╔╗║ ║╔╗║║║   ║║║╔═╝║║║║ ║╔╗║║╔═╗║╚═╗╔═╝║╔╗║║╔═╗║ |\n| ║╚╝║  ║╚╝║ ║║║║║╚══╗║╚╝║  ║╚╝║ ║║║║║╚═╝║  ║║  ║║║║║╚═╝║ |\n| ║╔╗║  ║╔╗║ ║║║║║╔═╗║║╔╗║  ╚═╗║ ║║║║╚╗╔╗║  ║║  ║║║║║╔══╝ |\n| ║║║╚═╗║║║║╔╝║║║║╚═╝║║║║╚═╗ ╔╝║╔╝║║║ ║║║║  ║║  ║╚╝║║║    |\n| ╚╝╚══╝╚╝╚╝╚═╝╚╝╚═══╝╚╝╚══╝ ╚═╝╚═╝╚╝ ╚╝╚╝  ╚╝  ╚══╝╚╝    | \n+──────────────────────────────────────────────────────── +")
 
-def load_def(filename):
-    with open(filename, 'rb') as f:
-        return pickle.load(f)
+print(Style.BRIGHT + Fore.YELLOW)
+print(' '*7 ,"-" * 42)
+print(' '*7 ,'| Выберите действие:' , " " * 19 , "|")
+print(' '*7 ,"-" * 42)
+print(' '*7 ,'| 1. Сложение' , " " * 26 , "|")
+print(' '*7 ,"-" * 42)
+print(' '*7 ,'| 2. Вычитание' , " " * 25 , "|")
+print(' '*7 ,"-" * 42)
+print(' '*7 ,'| 3. Умножение'  , " " * 25 , "|")
+print(' '*7 ,"-" * 42)
+print(' '*7 ,'| 4. Деление'  , " " * 27 , "|")
+print(' '*7 ,"-" * 42)
+print(' '*7 ,'| 5. Возведение'  , " " * 24, "|")
+print(' '*7 ,"-" * 42)
+print(' '*7 ,'| 6. Просмотр результата' , " " * 15, "|")
+print(' '*7 ,"-" * 42)
 
+math = int(input("        -> "))
 
-obj1 = MyClass(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
-obj2 = MyClass(2, 'Sed euismod, nisl vitae tincidunt lacinia, nisi nisi ullamcorper nisi, in aliquet massa nisl eget metus.')
-obj3 = MyClass(3, 'Proin euismod, augue nec aliquet malesuada, nisl velit egestas velit, eget euismod libero odio vitae lectus')
+class Calculator:
+    def __init__(self):
+        self.result = None
 
+    def add(self, a, b):
+        self.result = a + b
+        return self.result
 
-save_def(obj1, 'obj1.pkl')
-save_def(obj2, 'obj2.pkl')
-save_def(obj3, 'obj3.pkl')
+    def sub(self, a, b):
+        self.result = a - b
+        return self.result
 
-loaded_obj1 = load_def('obj1.pkl')
-loaded_obj2 = load_def('obj2.pkl')
-loaded_obj3 = load_def('obj3.pkl')
+    def mlp(self, a, b):
+        self.result = a * b
+        return self.result
 
+    def dvd(self, a, b):
+        if b == 0:
+            raise ValueError("На ноль делить нельзя, УЧИ МАТЕМАТИКУ !!!")
+        self.result = a / b
+        return self.result
 
-print(loaded_obj1.attribute1, loaded_obj1.attribute2)
-print(loaded_obj2.attribute1, loaded_obj2.attribute2)
-print(loaded_obj3.attribute1, loaded_obj3.attribute2)
+    def sqr(self, a, b):
+        self.result = a ** b
+        return self.result
+
+calc = Calculator()
+
+if math == 1:
+    a = int(input("        A -> "))
+    b = int(input("        B -> "))
+    result = calc.add(a,b)
+elif math == 2:
+    a = int(input("        A -> "))
+    b = int(input("        B -> "))
+    result = calc.sub(a,b)
+elif math == 3:
+    a = int(input("        A -> "))
+    b = int(input("        B -> "))
+    result = calc.mlp(a,b)
+elif math == 4:
+    a = int(input("        A -> "))
+    b = int(input("        B -> "))
+    result = calc.dvd(a,b)
+elif math == 5:
+    a = int(input("        A -> "))
+    b = int(input("        B -> "))
+    result = calc.sqr(a,b)
+elif math == 6:
+    try:
+        loaded_calc = pickle.load(open('calc.pkl', 'rb'))
+        result = loaded_calc.result
+    except FileNotFoundError:
+        print("Нет сохраненного результата.")
+else:
+    print("Неверный выбор.")
+
+print(Style.BRIGHT + Fore.WHITE)
+print("        Результат -> ", result)
+print("  ")
+
+# Сохранение результата в файл
+pickle.dump(calc, open('calc.pkl', 'wb'))
